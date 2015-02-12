317685b8ec82296fe3fa1b2ff6892ad83b04c39d
gabbydgab <gamba@gabbydgab.com>
2015-02-12 13:18:02 +0800

    updated license information

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -3,7 +3,7 @@
 /**
 * The MIT License
 *
-* Copyright (c) 2012-2014, contributors of Mega Mitch Credit, Inc. software project.
+* Copyright (c) 2012-2015, Mega Mitch, Inc.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -23,4 +23,4 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 */
-return array();
+return [];
